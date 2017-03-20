73a457f53dcf7654fd87acfe2af852b14c7d9e65
Amanda <ahh6007@uncw.edu>
2017-03-20 12:07:52 -0400

    Curved text can rotate

--- PanPractice/ViewController.swift
+++ PanPractice/ViewController.swift
@@ -11,68 +11,37 @@ import UIKit
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
   
   @IBOutlet weak var circle: UIImageView!
-  var rotation = CGFloat(0)
-  var label: DGCurvedLabel!
-  var startTransform: CGAffineTransform?
-  var deltaAngle: Float = 0
-  var prevPoint: CGPoint = CGPoint.zero
-  var panGestureRecognizer: UIPanGestureRecognizer!
-  @IBOutlet weak var straightLabel: UILabel!
-  @IBOutlet var straightLabelPanRecognizer: UIPanGestureRecognizer!
   
   override func viewDidLoad() {
     let label = createOuterLabel()
-    circle.addSubview(label)
-    panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panned))
-    panGestureRecognizer.delegate = self
-    label.addGestureRecognizer(panGestureRecognizer)
-    label.isUserInteractionEnabled = true
-
+    createRotateGestureRecognizer(targetView: label)
+    view.addSubview(label)
   }
   
   func createOuterLabel() -> DGCurvedLabel {
     let font = UIFont (name: "Arial" , size: CGFloat(22))
-    let stringAttributes = [NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: font]
+    let stringAttributes = [NSForegroundColorAttributeName: UIColor.black, NSFontAttributeName: font]
       let curvedLabel: DGCurvedLabel = DGCurvedLabel(frame: self.circle.bounds)
       curvedLabel.attributedText = NSAttributedString(string: "I am testing a label", attributes: stringAttributes)
       curvedLabel.radius = circle.frame.size.width/3
-      curvedLabel.rotation = self.rotation
+      curvedLabel.rotation = 0
       curvedLabel.textInside = false
+      curvedLabel.isUserInteractionEnabled = true
       return curvedLabel
   }
 
-  @IBAction func straightLabelPanned(_ sender: UIPanGestureRecognizer) {
-    let currentPoint = sender.location(in: self.circle)
-    let center = sender.view!.center
-    let angle =  atan2f(Float(currentPoint.y) - Float(center.y), Float(currentPoint.x) - Float(center.x)) - atan2f(Float(prevPoint.y) - Float(center.y), Float(prevPoint.x) - Float(center.x))
-    prevPoint = sender.location(in: self.circle)
-    deltaAngle += angle
-    self.straightLabel.transform = startTransform!.rotated(by: CGFloat(deltaAngle))
+  func createRotateGestureRecognizer(targetView:DGCurvedLabel) {
+    let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(self.handleRotate(_:)))
+    targetView.addGestureRecognizer(rotateGesture)
+    rotateGesture.delegate = self
+  }
+  
+  func handleRotate(_ recognizer : UIRotationGestureRecognizer) {
+    
+    recognizer.view!.transform = recognizer.view!.transform.rotated(by: recognizer.rotation)
+    recognizer.rotation = 0
+  }
 
-  }
-  
-  func panned(recognizer: UIPanGestureRecognizer) {
-   
-    
-    let currentPoint = recognizer.location(in: self.circle)
-    let center = recognizer.view!.center
-    let angle =  atan2f(Float(currentPoint.y) - Float(center.y), Float(currentPoint.x) - Float(center.x)) - atan2f(Float(prevPoint.y) - Float(center.y), Float(prevPoint.x) - Float(center.x))
-    prevPoint = recognizer.location(in: self.circle)
-    deltaAngle += angle
-    self.label.transform = startTransform!.rotated(by: CGFloat(deltaAngle))
-    
-    
-  }
-  
-  func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-    
-    if gestureRecognizer == self.panGestureRecognizer {
-      startTransform = self.label.transform
-    }
-    else if gestureRecognizer == straightLabelPanRecognizer {
-      startTransform = self.straightLabel.transform
-    }
-    return true
-  }
+
 }
 
